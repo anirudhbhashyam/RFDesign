3aed7906ea25e341c8c008b3493439068fe7cecf
bhashyam.anirudh <bhashyam.anirudh@gmail.com>
2023-03-21 15:00:13 +0100

    Fixed Jax indexing.

--- hallucination/models/alphafold/alphafold/model/modules.py
+++ hallucination/models/alphafold/alphafold/model/modules.py
@@ -1730,8 +1730,7 @@ class EmbeddingsAndEvoformer(hk.Module):
                                           True,
                                           name='prev_msa_first_row_norm')(
                                               batch['prev_msa_first_row'])
-        msa_activations = jax.ops.index_add(msa_activations, 0,
-                                            prev_msa_first_row)
+        msa_activations.at[0].set(prev_msa_first_row)
 
       if 'prev_pair' in batch:
         pair_activations += hk.LayerNorm([-1],
